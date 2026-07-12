059367b6623ec9f4291e69fe3545e7e5d45d8bf4
somilbansal-2004 <somilbansal2004@gmail.com>
2026-07-12 17:35:05 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
-  return {
+  return {base:'/nexora.1.0/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
